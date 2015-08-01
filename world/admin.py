bae8325aa2cb17e6eff925bb107b118453bebfc5
Fei Zhang <fei2zhang@gmail.com>
2015-08-02 09:52:01 +1000

    update gitignore admin.py

--- world/admin.py
+++ world/admin.py
@@ -1,12 +1,13 @@
+#See https://docs.djangoproject.com/en/1.8/ref/contrib/gis/tutorial/#introduction
+
 #from django.contrib import admin
 
-# Register your models here.
 from django.contrib.gis import admin
 from models import WorldBorder
 
-admin.site.register(WorldBorder, admin.GeoModelAdmin)
-
-# https://docs.djangoproject.com/en/1.8/ref/contrib/gis/tutorial/#introduction
-#admin.site.register(WorldBorder, admin.OSMGeoAdmin)
+# Register your models here.
+#admin.site.register(WorldBorder, admin.GeoModelAdmin)
+# OR
+admin.site.register(WorldBorder, admin.OSMGeoAdmin)
 
 
